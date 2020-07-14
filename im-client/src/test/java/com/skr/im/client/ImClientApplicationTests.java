1a3b7ecc710649c85030d0be4e7a95c5913cc610
mengqingwen <mengqingwen@hetao101.com>
2020-07-14 15:31:45 +0800

    pom init

---
+++ im-client/src/test/java/com/skr/im/client/ImClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr.im.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
