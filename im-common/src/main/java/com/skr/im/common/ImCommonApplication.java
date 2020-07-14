c4e4fbd5a499c1e66cff3d3fdd279c15449c4ebc
mengqingwen <mengqingwen@hetao101.com>
2020-07-14 15:26:44 +0800

    pom init

---
+++ im-common/src/main/java/com/skr/im/common/ImCommonApplication.java
@@ -0,0 +1,13 @@
+package com.skr.im.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImCommonApplication.class, args);
+    }
+
+}
