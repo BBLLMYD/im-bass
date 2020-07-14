b7b5560fa016adbf47b86722ccaf59d1fe4fe3d3
BBLLMYD <mmzw0930@gmail.com>
2020-07-14 15:31:45 +0800

    pom init

---
+++ im-client/src/main/java/com/skr/im/client/ImClientApplication.java
@@ -0,0 +1,13 @@
+package com.skr.im.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImClientApplication.class, args);
+    }
+
+}
