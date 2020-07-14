d81960f05498010fccac41499548ba59bae2abda
BBLLMYD <mmzw0930@gmail.com>
2020-07-14 15:26:44 +0800

    pom init

---
+++ im-common/src/test/java/com/skr/im/common/ImCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr.im.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
