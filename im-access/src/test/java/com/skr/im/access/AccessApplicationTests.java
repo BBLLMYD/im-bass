7583c51fec7af3d9fcc6c308bfa8bfc9dc23d5be
mengqingwen <mengqingwen@hetao101.com>
2020-07-14 14:08:49 +0800

    pom init

---
+++ im-access/src/test/java/com/skr/im/access/AccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr.im.access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
